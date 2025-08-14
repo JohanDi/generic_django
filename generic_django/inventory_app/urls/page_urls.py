4384c9b4d45510bc355aa1fdf12553b7bfda43b2
Johan Diels <diels.johan@gmail.com>
2025-08-14 18:05:04 +0200

    Implemented views for compartment model

--- generic_django/inventory_app/urls/page_urls.py
+++ generic_django/inventory_app/urls/page_urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path("section/create/", pages.SectionCreateView.as_view(), name="section-create"),
     path("section/update/<int:pk>/", pages.SectionUpdateView.as_view(), name="section-update"),
     path("section/delete//<int:pk>/", pages.SectionDeleteView.as_view(), name="section-delete"),
+    path("compartment/list/", pages.CompartmentListView.as_view(), name="compartment-list"),
+    path("compartment/create/", pages.CompartmentCreateView.as_view(), name="compartment-create"),
+    path("compartment/update/<int:pk>/", pages.CompartmentUpdateView.as_view(), name="compartment-update"),
+    path("compartment/delete/<int:pk>/", pages.CompartmentDeleteView.as_view(), name="compartment-delete"),
 ]
--- generic_django/inventory_app/views/pages.py
+++ generic_django/inventory_app/views/pages.py
@@ -4,11 +4,11 @@ from django.contrib import messages
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-from ..models import Section  # Replace with your actual model
+from ..models import Section, Compartment
 
 class SectionListView(LoginRequiredMixin, ListView):
     model = Section
-    template_name = 'inventory_app/section_list.html'
+    template_name = 'generic_table_page.html'
     paginate_by = 10
 
     def get_queryset(self):
@@ -35,7 +35,7 @@ class SectionListView(LoginRequiredMixin, ListView):
 
 class SectionCreateView(LoginRequiredMixin, CreateView):
     model = Section
-    template_name = 'inventory_app/section_form.html'
+    template_name = 'generic_form_page.html'
     fields = ['name', 'description']
     success_url = reverse_lazy('inventory_app:section-list')
 
@@ -45,7 +45,7 @@ class SectionCreateView(LoginRequiredMixin, CreateView):
 
 class SectionUpdateView(LoginRequiredMixin, UpdateView):
     model = Section
-    template_name = 'inventory_app/section_form.html'
+    template_name = 'generic_form_page.html'
     fields = ['name', 'description']
     success_url = reverse_lazy('inventory_app:section-list')
 
@@ -72,4 +72,75 @@ class SectionDeleteView(LoginRequiredMixin, DeleteView):
     def get_success_url(self):
 
         messages.success(self.request, 'Section deleted successfully.')
+        return super().get_success_url()
+
+class CompartmentListView(LoginRequiredMixin, ListView):
+    model = Compartment
+    template_name = 'generic_table_page.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        order_by = self.request.GET.get('order_by', 'name')
+        valid_fields = ['name', 'description']
+        field = order_by.lstrip('-')
+        if field in valid_fields:
+            queryset = queryset.order_by(order_by)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['verbose_name'] = Compartment._meta.verbose_name
+        context['verbose_name_plural'] = Compartment._meta.verbose_name_plural
+        context['headers'] = [
+            {'name': 'Name', 'field': 'name'},
+            {'name': 'Description', 'field': 'description'},
+            {'name': 'Section', 'field': 'section.name'},
+        ]
+        context['create_url'] = 'inventory_app:compartment-create'
+        context['update_url'] = 'inventory_app:compartment-update'
+        context['delete_url'] = 'inventory_app:compartment-delete'
+        return context
+
+class CompartmentCreateView(LoginRequiredMixin, CreateView):
+    model = Compartment
+    template_name = 'generic_form_page.html'
+    fields = ['name', 'description', 'section']
+    success_url = reverse_lazy('inventory_app:compartment-list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_update'] = False
+        context['verbose_name'] = 'compartment'
+        return context
+
+    def get_success_url(self):
+        messages.success(self.request, 'Compartment created successfully.')
+        return super().get_success_url()
+
+class CompartmentUpdateView(LoginRequiredMixin, UpdateView):
+    model = Compartment
+    template_name = 'generic_form_page.html'
+    fields = ['name', 'description']
+    success_url = reverse_lazy('inventory_app:compartment-list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_update'] = True
+        context['verbose_name'] = 'compartment'
+        return context
+
+class CompartmentDeleteView(LoginRequiredMixin, DeleteView):
+    model = Compartment
+    # template_name = 'inventory_app/section_confirm_delete.html'
+    success_url = reverse_lazy('inventory_app:compartment-list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_delete'] = True
+        return context
+
+    # Add a success message or confirmation if needed
+    def get_success_url(self):
+        messages.success(self.request, 'Compartment deleted successfully.')
         return super().get_success_url()
